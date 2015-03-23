08951ce26598f79897fc50056f2c7ad1c40a15ce
jonas <jonas,g,svensson@gmail.com>
2015-03-23 13:23:39 +0100

    New String

--- app/src/main/java/com/test/sejakth/githubtest/MainActivity.java
+++ app/src/main/java/com/test/sejakth/githubtest/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    private String a;
+    private String s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
